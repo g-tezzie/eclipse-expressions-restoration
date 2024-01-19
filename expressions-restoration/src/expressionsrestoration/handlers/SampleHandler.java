9a34793c3652db66c6c94c09bcf1215de39afabf
g-tezzie <g.tezzie@gmail.com>
2024-01-20 03:18:59 +0900

    append constant expression into Expressions view

--- expressions-restoration/src/expressionsrestoration/handlers/SampleHandler.java
+++ expressions-restoration/src/expressionsrestoration/handlers/SampleHandler.java
@@ -3,14 +3,21 @@ package expressionsrestoration.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IExpressionManager;
+import org.eclipse.debug.core.model.IWatchExpression;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 public class SampleHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IExpressionManager expressionManager = DebugPlugin.getDefault().getExpressionManager();
+		IWatchExpression newWatchExpression = expressionManager.newWatchExpression("piyo");
+		expressionManager.addExpression(newWatchExpression);
+		
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(
 				window.getShell(),
