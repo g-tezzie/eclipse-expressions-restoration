267ab554cff4a738632b47561dd36270f868d64e
g-tezzie <g.tezzie@gmail.com>
2024-01-20 20:07:47 +0900

    file in/out with fixed filename

--- expressions-restoration/src/expressionsrestoration/handlers/SampleHandler.java
+++ expressions-restoration/src/expressionsrestoration/handlers/SampleHandler.java
@@ -1,10 +1,25 @@
 package expressionsrestoration.handlers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IExpressionManager;
+import org.eclipse.debug.core.model.IExpression;
 import org.eclipse.debug.core.model.IWatchExpression;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -14,15 +29,54 @@ public class SampleHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+//		ResourceSelectionDialog dialog = new ResourceSelectionDialog(getShell(), rootResource, msg);
+//		dialog.setInitialSelections(selectedResources);
+//		dialog.open();
+//		return dialog.getResult();
+
 		IExpressionManager expressionManager = DebugPlugin.getDefault().getExpressionManager();
 		IWatchExpression newWatchExpression = expressionManager.newWatchExpression("piyo");
 		expressionManager.addExpression(newWatchExpression);
-		
+
+		@SuppressWarnings("unused")
+		IExpression[] expressions = expressionManager.getExpressions();
+
+		Stream<String> map = Arrays.stream(expressions).map(r -> r.getExpressionText());
+
+		Path path = Path.of("hoge");
+		OpenOption options = StandardOpenOption.CREATE_NEW;
+		try {
+			OutputStream newOutputStream = Files.newOutputStream(path, options);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(newOutputStream);
+			Object[] array = map.toArray();
+			List<Object> from = List.of(array);
+			objectOutputStream.writeObject(from);
+			newOutputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			InputStream newInputStream = Files.newInputStream(path, StandardOpenOption.READ);
+			Stream<String> map2 = extracted(newInputStream);
+			map2.forEach(x -> expressionManager.addExpression(expressionManager.newWatchExpression(x)));
+		} catch (IOException | ClassNotFoundException | ClassCastException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Expressions-restoration",
-				"Hello, Eclipse world");
+		MessageDialog.openInformation(window.getShell(), "Expressions-restoration", "Hello, Eclipse world");
 		return null;
+	}
+
+	public Stream<String> extracted(InputStream newInputStream)
+			throws IOException, ClassNotFoundException, ClassCastException {
+		ObjectInputStream objectInputStream = new ObjectInputStream(newInputStream);
+		Object readObject = objectInputStream.readObject();
+		Stream<String> map2 = List.copyOf((Collection<?>) readObject).stream().map((x) -> (String) x);
+		return map2;
 	}
 }
